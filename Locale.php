ce450c2bfe6836cfe6706f6dfbf3cd7a07b5d76a
Yannick <yannick.lalleau@yrycom.com>
2017-11-22 13:30:37 +0100

    Add attribut at Locale

--- Locale.php
+++ Locale.php
@@ -5,29 +5,52 @@ namespace Harmony2;
 class Locale
 {
 
-    public static function start($leslangues = []) {
-        include_once __DIR__.'/../../config/locale.php';
+	public static $LOCALE = '';
+	public static $LOCALE_DATE_FORMAT = '';
+	public static $LOCALE_DATETIME_FORMAT= '';
 
-        //chargement des fonctions nécessaires aux traductions
-        include_once __DIR__.'/locales/functions.php';
+	public static $LOCALE_DIR = '';
+	public static $LOCALE_DOMAIN = '';
 
-        $locale= LOCALE.".UTF-8";
-        putenv("LANGUAGE=");
-        putenv("LANG=".$locale);
-        setlocale(LC_ALL, $locale);
-				setlocale(LC_NUMERIC,'C');
+	public static function start($leslangues = [])
+	{
 
-        if(defined("LOCALE") and in_array(strtolower(LOCALE), $leslangues)) {
-            if (($value = bindtextdomain(LOCALE_DOMAIN, LOCALE_DIR)) != LOCALE_DIR) {
-                die('erreur dico bindtextdomain : ' . $value);
-            }
-            if (($value = textdomain(LOCALE_DOMAIN)) != LOCALE_DOMAIN) {
-                die('erreur dico : textdomain : ' . $value);
-            }
-        }
-    }
+		if (file_exists(__DIR__ . '/../../config/locale.php'))
+			include_once __DIR__ . '/../../config/locale.php';
 
-    public static function getLanguage() {
-        return explode('_',LOCALE)[0];
-    }
+		if(defined('LOCALE'))
+			self::$LOCALE = LOCALE;
+		if(defined('LOCALE_DATE_FORMAT'))
+			self::$LOCALE_DATE_FORMAT = LOCALE_DATE_FORMAT;
+		if(defined('LOCALE'))
+			self::$LOCALE_DATETIME_FORMAT = LOCALE_DATETIME_FORMAT;
+
+		if(defined('LOCALE'))
+			self::$LOCALE_DIR = LOCALE_DIR;
+		if(defined('LOCALE'))
+			self::$LOCALE_DOMAIN = LOCALE_DOMAIN;
+
+		//chargement des fonctions nécessaires aux traductions
+		include_once __DIR__ . '/locales/functions.php';
+
+		$locale = self::$LOCALE . ".UTF-8";
+		putenv("LANGUAGE=");
+		putenv("LANG=" . $locale);
+		setlocale(LC_ALL, $locale);
+		setlocale(LC_NUMERIC, 'C');
+
+		if (defined("LOCALE") and in_array(strtolower(self::$LOCALE), $leslangues)) {
+			if (($value = bindtextdomain(self::$LOCALE_DOMAIN, self::$LOCALE_DIR)) != self::$LOCALE_DIR) {
+				die('erreur dico bindtextdomain : ' . $value);
+			}
+			if (($value = textdomain(self::$LOCALE_DOMAIN)) != self::$LOCALE_DOMAIN) {
+				die('erreur dico : textdomain : ' . $value);
+			}
+		}
+	}
+
+	public static function getLanguage()
+	{
+		return explode('_', self::$LOCALE)[0];
+	}
 }
